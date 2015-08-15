05c0c8dec45f86bd948e1097d715861027bdc55e
Gustavo Arcanjo Silva <gustavoarcanjo@gmail.com>
2015-08-15 18:46:48 -0300

    Flying dots

---
+++ Assets/Flying2DUserControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (FlyingCharacter2D))]
+public class Flying2DUserControl : MonoBehaviour
+{
+	private FlyingCharacter2D m_Character;
+	
+	private void Awake()
+	{
+		m_Character = GetComponent<FlyingCharacter2D>();
+	}
+	
+	private void FixedUpdate()
+	{
+		// Read the inputs.
+		float h = CrossPlatformInputManager.GetAxis("Horizontal");
+		float v = CrossPlatformInputManager.GetAxis("Vertical");
+		// Pass all parameters to the character control script.
+		m_Character.Move(h, v);
+	}
+}
