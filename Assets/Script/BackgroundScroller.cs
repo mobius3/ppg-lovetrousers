7131ab80d03c99eb6e995377dcc0590823cc7bfb
Gustavo Arcanjo Silva <gustavoarcanjo@gmail.com>
2015-08-16 00:52:59 -0300

    Blossom moving

--- Assets/Script/BackgroundScroller.cs
+++ Assets/Script/BackgroundScroller.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BackgroundScroller : MonoBehaviour {
 
     static public float Speed = 1.0f;
+	static public float Ypos = 0f;
     public float Factor;
     private float pos;
 
@@ -15,5 +16,6 @@ public class BackgroundScroller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         GetComponent<Renderer>().material.mainTextureOffset += new Vector2(Speed * Factor * Time.deltaTime, 0);
+		//transform.position = Ypos * Factor * Vector3.up;
 	}
 }
