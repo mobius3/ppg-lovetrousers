9d75284fed048c0a9b89ef6ee4cffc9f38d33d24
Leonardo <leonardo.guilherme@gmail.com>
2015-08-16 01:28:03 -0300

    Adding other girls

---
+++ Assets/Script/PPGController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PPGController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, -Input.GetAxis("Horizontal") * 3);
+        transform.position += transform.up * Input.GetAxis("Vertical");
+        Debug.Log("x" + Input.GetAxis("Horizontal") + "y" + transform.right + "z" + transform.up);
+        BackgroundScroller.Speed = -Input.GetAxis("Vertical") * transform.right.y;
+        BackgroundScroller.Ypos = transform.position.y;
+	}
+}
