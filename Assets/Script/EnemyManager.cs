0e7923f54c49feb8c982b5f30098314aa9931de1
Leonardo <leonardo.guilherme@gmail.com>
2015-08-16 15:35:18 -0300

    Adding enemy spawning rules

---
+++ Assets/Script/EnemyManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyManager : MonoBehaviour {
+
+    public Transform follow;
+    public GameObject cerebroLoucoPrefab;
+    public List<GameObject> enemies = new List<GameObject>();
+    public int maxEnemies;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public IEnumerator SpawnEnemy()
+    {
+        while (enemies.Count < maxEnemies)
+        {
+            yield return new WaitForSeconds(1.0f);
+            GameObject enemyObj = GameObject.Instantiate(cerebroLoucoPrefab);
+            enemyObj.GetComponent<Enemy>().follow = follow;
+            //enemyObj.transform.position = follow.position + follow.up * 3;
+            enemies.Add(enemyObj);
+        }
+    }
+}
